35e8be885d055e9a42e9fd222ec520542e7eb8bd
cuiyi05 <cuiyi05@58.com>
2022-10-12 17:46:41 +0800

    feat: business-components 逻辑修改

--- packages/business-components/index.js
+++ packages/business-components/index.js
@@ -1,7 +1,9 @@
 const components = require('@my-component/components')
+const utils = require('@my-component/utils')
 
 function businessComponent() {
   components()
+  utils()
   console.log('businessComponent')
 }
 
